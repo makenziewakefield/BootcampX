30286287f53715759727ee56cdceb4c9c1a910e2
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-05-29 16:23:21 -0600

    Add data

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,8 @@
+ cohort_name | student_count 
+-------------+---------------
+ FEB12       |            18
+ APR09       |            19
+ JUN04       |            19
+ NOV19       |            22
+ SEP24       |            22
+(5 rows)
